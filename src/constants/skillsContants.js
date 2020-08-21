3111788458ef0d3cb4a176210c955a191d1e85b6
melinamejia95 <melinamejia95@gmail.com>
2020-08-20 19:16:52 -0500

    Adding skills percentages

---
+++ src/constants/skillsContants.js
@@ -0,0 +1,30 @@
+export const skillsPreset = [
+  {
+    title: 'HTML',
+    percentage: '80'
+  },
+  {
+    title: 'CSS',
+    percentage: '80'
+  },
+  {
+    title: 'JavaScript',
+    percentage: '65'
+  },
+  {
+    title: 'React',
+    percentage: '70'
+  },
+  {
+    title: 'React Native',
+    percentage: '40'
+  },
+  {
+    title: 'Git',
+    percentage: '60'
+  },
+  {
+    title: 'Git',
+    percentage: '60'
+  }
+];
