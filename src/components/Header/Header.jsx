97ca39bbcf3433fdddc2d21def29643e508ac836
melinamejia95 <melinamejia95@gmail.com>
2020-08-24 19:18:32 -0500

    Mobile Nav added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import './Header.scss';
@@ -6,27 +6,54 @@ import { Dropdown } from '../Dropdown';
 import photo from '../../assets/images/me.jpg';
 
 const Header = () => {
+  const [mobileNavVisibility, setMobileNavVisibility] = useState(false);
+
+  const toogleMobileNavVisibility = () => {
+    setMobileNavVisibility(!mobileNavVisibility);
+  }
+
   return (
-    <div className='header-container'>
-      <img src={photo} alt="Melina's photo" />
-      <div className='img-container'></div>
-      <nav>
-        <ul>
-          <li>
-            <Link to='/'>About</Link>
+    <>
+      {mobileNavVisibility && <div className='backdrop' onClick={toogleMobileNavVisibility}></div>}
+      <div className='header-container'>
+        <button onClick={toogleMobileNavVisibility}>
+          <img src={photo} alt="Melina's photo" />
+        </button>
+        <div className='img-container'></div>
+        <nav className='main-nav'>
+          <ul>
+            <li>
+              <Link to='/'>About</Link>
+            </li>
+            <li>
+              <Link to='/talks'>Talks</Link>
+            </li>
+            <li>
+              <Link to='/art'>Art</Link>
+            </li>
+            <li>
+              <Dropdown />
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <nav className={`mobile-nav ${mobileNavVisibility ? 'open' : ''}`}>
+        <ul className="mobile-nav__items">
+          <li className="mobile-nav__item">
+            <Link to='/' onClick={toogleMobileNavVisibility}>About</Link>
           </li>
-          <li>
-            <Link to='/talks'>Talks</Link>
+          <li className="mobile-nav__item">
+            <Link to='/talks' onClick={toogleMobileNavVisibility}>Talks</Link>
           </li>
-          <li>
-            <Link to='/art'>Art</Link>
+          <li className="mobile-nav__item">
+            <Link to='/art' onClick={toogleMobileNavVisibility}>Art</Link>
           </li>
-          <li>
+          <li className="mobile-nav__item">
             <Dropdown />
           </li>
         </ul>
       </nav>
-    </div>
+    </>
   )
 }
 
