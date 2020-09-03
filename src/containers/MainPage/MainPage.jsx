e7f78f05c7068a053e628a086a3a330391864783
melinamejia95 <melinamejia95@gmail.com>
2020-09-02 21:15:47 -0500

    Languages section added

--- src/containers/MainPage/MainPage.jsx
+++ src/containers/MainPage/MainPage.jsx
@@ -34,6 +34,34 @@ const MainPage = () => {
           })}
         </div>
       </section>
+      <section className='languages-container'>
+        <h3>Languages</h3>
+        <p>
+          Those are the languages I speak and/or I'm learning
+        </p>
+        <div className='languages-card'>
+          <img src={photo} alt='Melina first language'/>
+          <div className='language-description'>
+            <h4>Spanish</h4>
+            <label>Native</label>
+          </div>
+        </div>
+        <div className='languages-card' id='english'>
+          <div className='language-description'>
+            <h4>English</h4>
+            <label>Level C1</label>
+          </div>
+          <img src={photo} alt='Melina first language'/>
+        </div>
+        <div class="clearfix"></div>
+        <div className='languages-card'>
+          <img src={photo} alt='Melina first language'/>
+          <div className='language-description'>
+            <h4>Korean</h4>
+            <label>Learning!</label>
+          </div>
+        </div>
+      </section>
     </div>
   )
 };
