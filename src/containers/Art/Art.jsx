48d8abd77a9094363d5b855b1d9bab618e11e780
melinamejia95 <melinamejia95@gmail.com>
2021-05-09 16:51:16 -0500

    Splide library added (WIP)

--- src/containers/Art/Art.jsx
+++ src/containers/Art/Art.jsx
@@ -1,15 +1,45 @@
 import React, { useState, useEffect } from 'react'
 import { useTranslation } from 'react-i18next'
-import "react-responsive-carousel/lib/styles/carousel.min.css"
-import { Carousel } from 'react-responsive-carousel'
+import { Splide, SplideSlide } from '@splidejs/react-splide'
+import '@splidejs/splide/dist/css/themes/splide-default.min.css'
+// import '@splidejs/splide/dist/css/themes/splide-skyblue.min.css';
 
 import styles from './Art.module.scss'
 import { Loader } from '../../components/Loader'
 import { artPreset } from '../../constants/artConstanst'
 
+const useWindowSize = () => {
+  // Initialize state with undefined width/height so server and client renders match
+  // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
+  const [windowSize, setWindowSize] = useState({
+    width: undefined,
+    height: undefined,
+  });
+  useEffect(() => {
+    // Handler to call on window resize
+    function handleResize() {
+      // Set window width/height to state
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    }
+    // Add event listener
+    window.addEventListener("resize", handleResize);
+    // Call handler right away so state gets updated with initial window size
+    handleResize();
+    // Remove event listener on cleanup
+    return () => window.removeEventListener("resize", handleResize);
+  }, []); // Empty array ensures that effect is only run on mount
+  return windowSize;
+}
+
 const Art = () => {
   const { t } = useTranslation()
+  const size = useWindowSize()
   const [loaderVisibility, setLoaderVisibility] = useState(true)
+
+  console.log(size)
 
   useEffect(() => {
     /* eslint-disable */
@@ -22,21 +52,51 @@ const Art = () => {
     <div className={styles.artContainer}>
       <h1>{t('artTitle')}</h1>
       <p>{t('artDescription')}</p>
-      {loaderVisibility ? (
+      <div className={styles.carouselContainer}>
+        <Splide
+          options={ {
+            rewind : true,
+            perPage: 3,
+            perMove: 1,
+            gap    : '1rem',
+          } }
+          className={styles.carousel}
+        >
+          {artPreset.map((img, index) => {
+              return (
+                <SplideSlide key={index}>
+                  <img className={styles.carouselImg} src={img.source} alt={img.caption}/>
+                </SplideSlide>
+            )
+          })}
+        </Splide>
+      </div>
+      {/* {loaderVisibility ? (
         <Loader type='Puff' color='#FFF' />
       ) : (
         <div className={styles.carouselContainer}>
-          <Carousel>
+          <Splide
+            options={ {
+              type   : 'loop',
+              perPage: size > 650 ? 3 : 2,
+              perMove: 1,
+              gap    : '1rem',
+              focus: 'center',
+              height: size > 650 ? 'auto' : '100%',
+              fixedWidth: '80%'
+            } }
+            className={styles.carousel}
+          >
             {artPreset.map((img, index) => {
-              return (
-                <div key={index}>
-                  <img src={img.source} alt={img.caption} />
-                </div>
+                return (
+                  <SplideSlide key={index}>
+                    <img className={styles.carouselImg} src={img.source} alt={img.caption}/>
+                  </SplideSlide>
               )
             })}
-          </Carousel>
+          </Splide>
         </div>
-      )}
+      )} */}
     </div>
   )
 }
